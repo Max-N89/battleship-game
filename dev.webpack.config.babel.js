f4dd32d39a8519688bfa1a6d77b1f217f7012f14
Max-N89 <4work.maxn@gmail.com>
2021-10-20 11:58:08 +0300

    Initial webpack configuration

---
+++ dev.webpack.config.babel.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+    target: "web",
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        static: false,
+        client: {
+            overlay: true,
+        },
+    },
+    entry: {
+        app: {
+            import: "./src/index.js",
+        },
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/i,
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader"
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "Battleship Game",
+            favicon: "./assets/icons/battleship-icon.png",
+        })
+    ]
+}
+
+module.exports = config;
--- readme.md
+++ readme.md
@@ -4,6 +4,10 @@ A game web application based on the
  [classic game](https://en.wikipedia.org/wiki/Battleship_(game)) of the same name
  built with [React](https://reactjs.org/) and related libraries.
 
+## Requirements
+
+- Node.js (at least v12.13.0)
+
 ## License
 
 Distributed under the [MIT license](https://opensource.org/licenses/MIT).
