fca88044cb03e7c4942db68f1480cd52a57a7b80
Max-N89 <4work.maxn@gmail.com>
2021-11-20 20:34:50 +0200

    Add gameAutoDeploy action creator

--- src/store/slices/game.js
+++ src/store/slices/game.js
@@ -1,5 +1,14 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit";
 
+import {
+    selectPlayerUndeployedShips,
+    selectPlayerAvailableDeploymentAnchors,
+} from "../game-selectors";
+
+import {DEPLOYMENT_DIRECTIONS} from "../../constants";
+
+const {HORIZONTAL, VERTICAL} = DEPLOYMENT_DIRECTIONS;
+
 const gameSlice = createSlice({
     name: "game",
     initialState: createInitState(),
@@ -64,6 +73,48 @@ export const {
     reset: gameReset,
     error: gameError,
 } = gameSlice.actions;
+
+export const gameAutoDeploy = playerId => (dispatch, getState) => {
+    const state = getState();
+    const undeployedShips = selectPlayerUndeployedShips(state, playerId);
+    const shipDescriptionToDeploy = undeployedShips.length === 1 ?
+        undeployedShips[0] :
+        undeployedShips[getRandomInteger(0, undeployedShips.length - 1)];
+    const deploymentDescription = {
+        length: shipDescriptionToDeploy.length,
+        direction: [HORIZONTAL, VERTICAL][getRandomInteger(0, 1)],
+    };
+
+    let availableDeploymentAnchors = selectPlayerAvailableDeploymentAnchors(
+        state,
+        playerId,
+        deploymentDescription.direction,
+        deploymentDescription.length
+    );
+
+    // switch deployment direction in case when there are no available spots to deploy with previous direction
+    if (!availableDeploymentAnchors.length) {
+        deploymentDescription.direction = deploymentDescription.direction === HORIZONTAL ?
+            VERTICAL :
+            HORIZONTAL;
+
+        availableDeploymentAnchors = selectPlayerAvailableDeploymentAnchors(
+            state,
+            playerId,
+            deploymentDescription.direction,
+            deploymentDescription.length
+        );
+    }
+
+    deploymentDescription.anchorCoords = availableDeploymentAnchors.length === 1 ?
+        availableDeploymentAnchors[0] :
+        availableDeploymentAnchors[getRandomInteger(0, availableDeploymentAnchors.length - 1)];
+
+    dispatch(gameDeploy(
+        playerID,
+        deploymentDescription
+    ));
+};
 
 export default gameSlice.reducer;
 
@@ -132,4 +183,10 @@ function createInitPlayersEntities() {
 
         return acc;
     }, {});
+}
+
+function getRandomInteger(min, max) {
+    const rand = min + Math.random() * (max + 1 - min);
+
+    return Math.floor(rand);
 }
