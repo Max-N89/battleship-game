15cab80b7bb2f40e8d65cb1c0611a7c24b2e4a7c
Max-N89 <4work.maxn@gmail.com>
2021-11-22 08:11:57 +0200

    Complement and rework store game slice selectors

--- src/store/game-selectors.js
+++ src/store/game-selectors.js
@@ -7,17 +7,33 @@ const {HORIZONTAL, VERTICAL} = DEPLOYMENT_DIRECTIONS;
 export const selectSettingsGridDescription = state => (
     state.game?.settings?.gridDescription
 );
+/*
+export const selectSettingsShipsIds = state => (
+    state.game?.settings?.ships?.ids
+);
+*/
+export const selectSettingsShips = state => (
+    state.game?.settings?.ships?.entities
+);
 
-export const selectPlayerOpponentId = (state, playerId) => (
-    state.game?.players?.entities?.[playerId]?.opponentId
+export const selectPlayersIds = state => (
+    state.game?.players?.ids
+);
+
+export const selectPlayer = (state, playerId) => (
+    state.game?.players?.entities?.[playerId]
 );
 
 export const selectPlayerDeploymentHistory = (state, playerId) => (
-    state.game?.players?.entities?.[playerId]?.deploymentHistory
+    selectPlayer(state, playerId)?.deploymentHistory
 );
 
 export const selectPlayerShotsHistory = (state, playerId) => (
-    state.game?.players?.entities?.[playerId]?.shotsHistory
+    selectPlayer(state, playerId)?.shotsHistory
+);
+
+export const selectPlayerOpponentId = (state, playerId) => (
+    selectPlayer(state, playerId)?.opponentId
 );
 
 export const selectPlayerOpponentShotsHistory = (state, playerId) => {
@@ -28,19 +44,111 @@ export const selectPlayerOpponentShotsHistory = (state, playerId) => {
     return selectPlayerShotsHistory(state, opponentId);
 };
 
+export const selectPlayerUndeployedShips = createSelector(
+    [
+        (state, playerId) => selectPlayerDeploymentHistory(state, playerId),
+        selectSettingsShips
+    ],
+    (deploymentHistory, settingsShips) => {
+        const deployedShipsIds = new Set(deploymentHistory.map(({shipId}) => shipId));
+
+        return Object.values(settingsShips).filter(({id}) => !deployedShipsIds.has(id));
+    }
+);
+
 export const selectPlayerDeploymentMap = createSelector(
     [
         (state, playerId) => selectPlayerDeploymentHistory(state, playerId),
+        selectSettingsShips,
         selectSettingsGridDescription,
     ],
-    (deploymentHistory, gridDescription) => {
-        if (!deploymentHistory || !gridDescription) return;
+    (deploymentHistory, settingsShips, gridDescription) => {
+        if (!deploymentHistory || !settingsShips || !gridDescription) return;
 
         const gridMap = new GridMap(gridDescription.width, gridDescription.height);
 
-        gridMap.addDeployments(deploymentHistory);
+        gridMap.addDeployments(deploymentHistory, settingsShips);
 
         return gridMap;
+    }
+);
+
+export const selectPlayerAvailableDeploymentAnchors = createSelector(
+    [
+        (state, playerId) => selectPlayerDeploymentMap(state, playerId),
+        (state, playerId, shipId) => selectSettingsShips(state)?.[shipId]?.length,
+        (state, playerId, shipId, deploymentDirection) => deploymentDirection,
+    ],
+    (deploymentMap, shipLength, deploymentDirection) => {
+        if (!deploymentMap || !shipLength || !deploymentDirection) return;
+
+        const deploymentAreas = [];
+
+        const {lastYCoord, lastXCoord} = deploymentMap;
+
+        /*
+            depending on deployment direction, iterate first over Y coordinates
+            or over X coordinates (for vertical) to pick continuous deployment areas
+            in horizontal or vertical direction respectively
+        */
+
+        let continuousDeploymentArea = [];
+        let firstLoopLastCoord, secondLoopLastCoord;
+
+        switch (deploymentDirection) {
+            case HORIZONTAL:
+                firstLoopLastCoord = lastYCoord;
+                secondLoopLastCoord = lastXCoord;
+
+                break;
+            case VERTICAL:
+                firstLoopLastCoord = lastXCoord;
+                secondLoopLastCoord = lastYCoord;
+
+                break;
+        }
+
+        for (let firstLoopCoord = 0; firstLoopCoord <= firstLoopLastCoord; firstLoopCoord++) {
+            for (let secondLoopCoord = 0; secondLoopCoord <= secondLoopLastCoord; secondLoopCoord++) {
+
+                let xCoord, yCoord;
+
+                switch (deploymentDirection) {
+                    case HORIZONTAL:
+                        xCoord = secondLoopCoord;
+                        yCoord = firstLoopCoord;
+
+                        break;
+                    case VERTICAL:
+                        xCoord = firstLoopCoord;
+                        yCoord = secondLoopCoord;
+
+                        break;
+                }
+
+                if (deploymentMap[yCoord][xCoord].isUndeployable){
+                    if (continuousDeploymentArea.length) {
+                        deploymentAreas.push(continuousDeploymentArea);
+                        continuousDeploymentArea = [];
+                    }
+
+                    continue;
+                }
+
+                continuousDeploymentArea.push({x: xCoord, y: yCoord});
+            }
+
+            if (continuousDeploymentArea.length) {
+                deploymentAreas.push(continuousDeploymentArea);
+            }
+
+            continuousDeploymentArea = [];
+        }
+
+        return deploymentAreas
+            .filter(area => area.length >= shipLength)
+            .map(area => area.slice(0, area.length - (shipLength - 1)))
+            .flat();
     }
 );
 
@@ -48,14 +156,15 @@ export const selectPlayerGameMap = createSelector(
     [
         (state, playerId) => selectPlayerDeploymentHistory(state, playerId),
         (state, playerId) => selectPlayerOpponentShotsHistory(state, playerId),
+        selectSettingsShips,
         selectSettingsGridDescription,
     ],
-    (playerDeploymentHistory, opponentShotsHistory, gridDescription) => {
-        if (!playerDeploymentHistory || !opponentShotsHistory || !gridDescription) return;
+    (playerDeploymentHistory, opponentShotsHistory, settingsShips, gridDescription) => {
+        if (!playerDeploymentHistory || !opponentShotsHistory || !settingsShips || !gridDescription) return;
 
         const gridMap = new GridMap(gridDescription.width, gridDescription.height);
 
-        gridMap.addDeployments(playerDeploymentHistory);
+        gridMap.addDeployments(playerDeploymentHistory, settingsShips);
         gridMap.addShots(opponentShotsHistory);
 
         return gridMap;
@@ -75,10 +184,7 @@ class GridMap extends Array {
                 const cell = {
                     isOccupied: false,
                     isShooted: false,
-                    coords: {
-                        x: xCoord,
-                        y: yCoord,
-                    },
+                    isUndeployable: false,
                 };
 
                 row.push(cell);
@@ -88,17 +194,31 @@ class GridMap extends Array {
         }
     }
 
-    addDeployments(deployments) {
-        deployments.forEach(deploymentDescription => {
+    get lastXCoord() {
+        return this[0].length - 1;
+    }
+
+    get lastYCoord() {
+        return this.length - 1;
+    }
+
+    addDeployments(deploymentHistory, settingsShips) {
+        deploymentHistory.forEach(deploymentDescription => {
             const {
                 anchorCoords: {
                     x: anchorXCoord,
                     y: anchorYCoord,
                 },
                 direction: deploymentDirection,
-                length: shipLength,
+                shipId,
             } = deploymentDescription;
 
+            const shipLength = settingsShips[shipId].length;
+
+            const isDeploymentHorizontal = deploymentDirection === HORIZONTAL;
+            const isDeploymentVertical = deploymentDirection === VERTICAL;
+
+            // cells which are become occupied
             switch (deploymentDirection) {
                 case HORIZONTAL: {
                     for (let xCoord = anchorXCoord; xCoord <= anchorXCoord + shipLength - 1; xCoord++) {
@@ -115,11 +235,34 @@ class GridMap extends Array {
                     break;
                 }
             }
+
+            // cells which are become undeployable
+            const {lastXCoord, lastYCoord} = this;
+
+            const fromXCoord = anchorXCoord === 0 ? anchorXCoord : anchorXCoord - 1;
+
+            const toXCoord = isDeploymentHorizontal ?
+                (anchorXCoord + shipLength > lastXCoord ? anchorXCoord + shipLength - 1 : anchorXCoord + shipLength) :
+                (anchorXCoord === lastXCoord ? anchorXCoord : anchorXCoord + 1);
+
+            const fromYCoord = anchorYCoord === 0 ? anchorYCoord : anchorYCoord - 1;
+
+            const toYCoord = isDeploymentVertical ?
+                (anchorYCoord + shipLength > lastYCoord ? anchorYCoord + shipLength - 1 : anchorYCoord + shipLength) :
+                (anchorYCoord === lastYCoord? anchorYCoord : anchorYCoord + 1);
+
+            for (let yCoord = fromYCoord; yCoord <= toYCoord; yCoord++) {
+                for (let xCoord = fromXCoord; xCoord <= toXCoord; xCoord++) {
+                    if (!this[yCoord][xCoord].isUndeployable) {
+                        this[yCoord][xCoord].isUndeployable = true;
+                    }
+                }
+            }
         });
     }
 
-    addShots(shots) {
-        shots.forEach(shotDescription => {
+    addShots(shotsHistory) {
+        shotsHistory.forEach(shotDescription => {
             const {
                 coords: {
                     x: shotXCoord,
@@ -127,7 +270,7 @@ class GridMap extends Array {
                 },
             } = shotDescription;
 
-            map[shotYCoord][shotXCoord].isShooted = true;
+            this[shotYCoord][shotXCoord].isShooted = true;
         });
     }
 }
