c0486c7271c4d865f8c657ebce47797f77025e5b
Max-N89 <4work.maxn@gmail.com>
2021-12-13 19:25:10 +0200

    Rework and rename Home component to SplashScreen

---
+++ src/components/splash-screen/splash-screen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import BackgroundVideo from "./background-video";
+import DemoGame from "./demo-game";
+import MessageBar from "../generic/message-bar";
+
+function SplashScreen(props) {
+    return (
+        <>
+            {/*<BackgroundVideo/>*/}
+            {/*<DemoGame/>*/}
+            {/*<MessageBar/>*/}
+            {/*<MessageBar/>*/}
+        </>
+    );
+}
+
+export default SplashScreen;
